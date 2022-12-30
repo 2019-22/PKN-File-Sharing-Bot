0ad75e0bf1a4d0398062678cd5f51e1544f061de
2019-22 <121666723+2019-22@users.noreply.github.com>
2022-12-30 14:23:06 +0300

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,27 @@
+#(©)Codexbotz
+
+from pyrogram import __version__
+from bot import Bot
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>○ Powered By : <a href='https://t.me/MGBotUpdates'>MG BOTS</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @pxlencodes\n○ Website : <a href='https://10bitclub.ml/'>10BITCLUB</a></b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
