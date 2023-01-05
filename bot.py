cfc95223ed554d8c291cb8887332aded94aa46ca
2019-22 <121666723+2019-22@users.noreply.github.com>
2023-01-06 00:18:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ class Bot(Client):
         #web-response
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "0.0.0.8050"
+        bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
